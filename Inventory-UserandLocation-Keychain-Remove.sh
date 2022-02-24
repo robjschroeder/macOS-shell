2afb0786a4316f15536865d9be608d307539c768
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 17:42:46 -0700

    Update Inventory-UserandLocation-Keychain-Remove.sh

--- Inventory-UserandLocation-Keychain-Remove.sh
+++ Inventory-UserandLocation-Keychain-Remove.sh
@@ -1,5 +1,17 @@
-﻿loggedInUser=$( ls -l /dev/console | awk '{print $3}' )
+#!/bin/bash
 
+# This script will remove the contents
+# of the logged in user's keychain to be
+# to be rebuilt upon next login.
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
+# Get the logged in user
+loggedInUser=$( ls -l /dev/console | awk '{print $3}' )
+
+# Remove the keychain contents of the user
 rm -f -r "/Users/$loggedInUser/Library/Keychains/"*
-shutdown -r now
+# Restart the computer in one minute
+shutdown -r +1
+
 exit
