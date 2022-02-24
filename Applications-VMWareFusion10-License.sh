15a20b2a190d7cd7d417eedc79cc1f32cb533a94
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 17:24:30 -0700

    Update Applications-VMWareFusion10-License.sh

--- Applications-VMWareFusion10-License.sh
+++ Applications-VMWareFusion10-License.sh
@@ -1,4 +1,12 @@
-﻿#!/bin/sh
+#!/bin/bash
+
+# This script will serialize VMWare
+# Fusion 10. Add valid serial number
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
+# Variables
 serial="serialNumber"
+
 export serial=$serial
 /Applications/VMware\ Fusion.app/Contents/Library/Initialize\ VMware\ Fusion.tool set "" "" ${serial}
