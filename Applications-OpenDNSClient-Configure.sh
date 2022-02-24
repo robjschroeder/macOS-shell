bf850a548ee06a34b413f21161b4a8855b264121
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 17:19:12 -0700

    Update Applications-OpenDNSClient-Configure.sh

--- Applications-OpenDNSClient-Configure.sh
+++ Applications-OpenDNSClient-Configure.sh
@@ -1,10 +1,18 @@
-﻿#!/bin/bash
+#!/bin/bash
+
+# This script will configure the OpenDNS
+# Roaming Client for macOS. 
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
+# Variables
 APIFingerprint="fingerprint"
 APIOrganizationID="OrgID"
 APIUserID="UserID"
 
-
+# Make the OpenDNS Roaming Client Directory
 mkdir -p "/Library/Application Support/OpenDNS Roaming Client/"
+# Build plist
 DATA='<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -20,3 +28,5 @@ DATA='<?xml version="1.0" encoding="UTF-8"?>
 </dict>
 </plist>'
 echo "$DATA" > "/Library/Application Support/OpenDNS Roaming Client/OrgInfo.plist"
+
+exit 0
