3115caf96cedda062fbdb97ce0a09036f41dcc38
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 17:15:01 -0700

    Update macOS-UnbindAD.sh

--- macOS-UnbindAD.sh
+++ macOS-UnbindAD.sh
@@ -1,1 +1,12 @@
-﻿dsconfigad -force -remove -u johndoe -p nopasswordhere
+#!/bin/bash
+
+# This script will unbind the mac
+# from AD. A valid username and 
+# password is NOT needed
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
+# Unbind with dsconfigad
+dsconfigad -force -remove -u johndoe -p nopasswordhere
+
+exit 0
