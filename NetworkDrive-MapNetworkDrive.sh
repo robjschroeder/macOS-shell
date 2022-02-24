c1c1b6408fcaa57a1d20e240b8c8313b9abc7729
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 17:29:48 -0700

    Update NetworkDrive-MapNetworkDrive.sh

--- NetworkDrive-MapNetworkDrive.sh
+++ NetworkDrive-MapNetworkDrive.sh
@@ -1,12 +1,23 @@
-﻿#!/bin/sh
+#!/bin/bash
+
+# This script can be used to mount
+# a network share. This is meant to be
+# used with a policy in Jamf Pro.
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
+# Jamf Pro script parameters: 
+# $4 = protocol to connect with (afp | smb)
+# $5 = address of the server, e.g. my.fileserver.com
+# $6 = name of the share to mount
 
 UID1=$(id -u $3)
     echo "UID: $UID1"
-protocol="$4" # This is the protocol to connect with (afp | smb)
+protocol="$4"
     echo "Protocol: $4"
-serverName="$5"   # This is the address of the server, e.g. my.fileserver.com
+serverName="$5"
     echo "Server: $5"
-shareName="$6"    # This is the name of the share to mount
+shareName="$6"
     echo "Sharename: $6"
       # Mount the drive 
         mount_script=`/usr/bin/osascript  > /dev/null << EOT
