a36c1610899cb5fae27f37e29a961c93447d009f
Robert Schroeder <robjschroeder@gmail.com>
2022-02-23 17:45:15 -0700

    Update Inventory-UserandLocation-Keychain-Remove-Remote.sh

--- Inventory-UserandLocation-Keychain-Remove-Remote.sh
+++ Inventory-UserandLocation-Keychain-Remove-Remote.sh
@@ -1,5 +1,15 @@
-﻿user=$4
+#!/bin/bash
 
+# This script will remove the contents
+# of the user's (defined by $4) keychain 
+# to be to be rebuilt upon next login.
+#
+# Updated: 2.23.2022 @ Robjschroeder
+#
+# $4 passed from Jamf Pro policy
+user=$4
+# Remove the contents of the keychain
 rm -f -r "/Users/$user/Library/Keychains/"*
-shutdown -r now
+# Reboot the computer in 1 minute
+shutdown -r +1
 exit
