ed958a2002fe3e0091423383eb96cf31adab83ab
Robert Schroeder <robjschroeder@gmail.com>
2022-03-02 21:41:48 -0700

    Update macOS-DownloadandUpgrade-Catalina.sh

--- macOS-DownloadandUpgrade-Catalina.sh
+++ macOS-DownloadandUpgrade-Catalina.sh
@@ -1,5 +1,12 @@
-﻿#!/bin/sh
+#!/bin/sh
 
+# Script used to download and install macOS 10.15.7 Catalina
+#
+# @robjschroeder
+
+# Reset any ingored software updates
 softwareUpdate --reset-ignored
+# Download 10.15.7
 softwareUpdate --fetch-full-installer --full-installer-version 10.15.7
+# Run the startosinstall binary
 /Applications/Install\ macOS\ Catalina.app/Contents/Resources/startosinstall --agreetolicense --forcequitapps
