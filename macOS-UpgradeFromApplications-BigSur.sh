614ca2e903871934976aee3caff1c50b21a3e5bc
Robert Schroeder <robjschroeder@gmail.com>
2022-03-02 21:35:29 -0700

    Update macOS-UpgradeFromApplications-BigSur.sh

--- macOS-UpgradeFromApplications-BigSur.sh
+++ macOS-UpgradeFromApplications-BigSur.sh
@@ -1,3 +1,9 @@
-﻿#!/bin/sh
+#!/bin/sh
+
+#
+# Upgrade macOS to Big Sur using the installer
+# located in /Applications/
+# @robjschroeder
+#
 
 /Applications/Install\ macOS\ Big\ Sur.app/Contents/Resources/startosinstall --agreetolicense --forcequitapps --rebootdelay 30
