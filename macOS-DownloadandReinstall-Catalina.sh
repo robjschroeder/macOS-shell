30c30f878ddb66202d02864db854ffecc2e0942b
Robert Schroeder <robjschroeder@gmail.com>
2022-03-02 21:44:52 -0700

    Update macOS-DownloadandReinstall-Catalina.sh

--- macOS-DownloadandReinstall-Catalina.sh
+++ macOS-DownloadandReinstall-Catalina.sh
@@ -1,5 +1,13 @@
-﻿#!/bin/sh
+#!/bin/sh
 
+# macOS Catalina Clean Install Script
+# Script will download macOS Catalina,
+# then run the installer with --eraseinstall
+# @robjschroeder
+
+# Reset any ignored updates
 softwareUpdate --reset-ignored
+# Fetch the latest macOS installer
 softwareUpdate --fetch-full-installer
+# Erase and Install
 /Applications/Install\ macOS\ Catalina.app/Contents/Resources/startosinstall --eraseinstall --agreetolicense --forcequitapps --newvolumename "MacHD"
