fb9115591df66d35f35b0b4262d26275419cb14e
Robert Schroeder <robjschroeder@gmail.com>
2022-03-02 21:33:37 -0700

    Update macOS-UpgradeFromApplications-Catalina.sh

--- macOS-UpgradeFromApplications-Catalina.sh
+++ macOS-UpgradeFromApplications-Catalina.sh
@@ -1,4 +1,11 @@
-﻿#!/bin/sh
+#!/bin/sh
+#
+# Upgrads macOS to Catalina using the startosinstall
+# from the installer located in /Applications
+# @robjschroeder
+#
 
+# Reset ignored software updates, if any
 softwareUpdate --reset-ignored
+# Run startosinstall
 /Applications/Install\ macOS\ Catalina.app/Contents/Resources/startosinstall --agreetolicense --forcequitapps
